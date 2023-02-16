11c484f5b2ddba04165cf27411fcedd7eda34cd4
Anh Khoa <dev.nakhoa@gmail.com>
2023-02-16 17:07:52 +0700

    fix bugs

--- lib/screens/Home/home_page.dart
+++ lib/screens/Home/home_page.dart
@@ -14,7 +14,7 @@ class HomePage extends StatelessWidget {
           width: MediaQuery.of(context).size.width * 1,
           decoration: const BoxDecoration(
               image: DecorationImage(
-                image: AssetImage("background.jpg"),
+                image: AssetImage("assets/background.jpg"),
                 fit: BoxFit.cover
               ),
           ),
@@ -22,9 +22,9 @@ class HomePage extends StatelessWidget {
             decoration: BoxDecoration(
               color: const Color(0xFF043150).withOpacity(0.5),
             ),
-            child: SingleChildScrollView(
+            child: const SingleChildScrollView(
               child: Column(
-                children: const [
+                children: [
                   LogoAndTitle(),
                   InputsOfHomeScreen(),
                 ],
--- lib/screens/Result/components/logo_and_title.dart
+++ lib/screens/Result/components/logo_and_title.dart
@@ -6,32 +6,30 @@ class LogoAndTitleForResultPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 30,vertical:50),
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
       child: Center(
-        child:Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset("logo.png",width: 120, height: 120),
-            const SizedBox(height:32),
-            const Text("Ping pong! Có kết quả rồi",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize:26,
-                fontWeight: FontWeight.w600
-              ),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset("assets/logo.png", width: 120, height: 120),
+          const SizedBox(height: 32),
+          const Text(
+            "Ping pong! Có kết quả rồi",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: Colors.white, fontSize: 26, fontWeight: FontWeight.w600),
+          ),
+          const SizedBox(height: 24),
+          const Text(
+            "Dưới đây là các điểm số tối thiểu cho các tín chỉ học phần không điểu kiện còn lại để đạt mục tiêu của bạn. Nếu bạn làm luận văn hoặc tiểu luận sẽ chia làm 3 trường hợp, nếu thiếu trường hợp nào thì có nghĩa ở trường hợp đó bạn không thể đạt mục tiêu.",
+            textAlign: TextAlign.justify,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
             ),
-            const SizedBox(height:24),
-            const Text("Dưới đây là các điểm số tối thiểu cho các tín chỉ học phần không điểu kiện còn lại để đạt mục tiêu của bạn. Nếu bạn làm luận văn hoặc tiểu luận sẽ chia làm 3 trường hợp, nếu thiếu trường hợp nào thì có nghĩa ở trường hợp đó bạn không thể đạt mục tiêu.",
-              textAlign: TextAlign.justify,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize:18,
-              ),
-            )
-          ],
-        )
-      ),
+          )
+        ],
+      )),
     );
   }
 }
